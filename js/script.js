4c5502968440690098ae59991777dbd8aa0c426f
jsun <jaysun054@gmail.com>
2021-08-28 13:54:37 -0700

    fix displays

--- js/script.js
+++ js/script.js
@@ -52,6 +52,7 @@ function guess(buttonId, choice) {
 
 function showScores() {
     window.alert("ended");
+    document.getElementById("qBox").style.display = "none";
 }
 
 var quiz = new Quiz(questions);
