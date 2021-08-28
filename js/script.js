4f9f722891fb74fe9b95b8600a4707b90d1586e0
jsun <jaysun054@gmail.com>
2021-08-27 23:17:01 -0700

    mock up

--- js/script.js
+++ js/script.js
@@ -5,9 +5,19 @@ start.onclick = function() {
     document.getElementById("one").style.display = "";
 }
 
-var next = document.getElementById("button");
+var next = document.getElementById("button-one");
 
 next.onclick = function() {
     document.getElementById("one").style.display = "none";
     document.getElementById("two").style.display = "";
+}
+
+var correct = document.getElementById("correct");
+
+correct.onclick = function() {
+    var qTwo = document.getElementById("two");
+    var elem = document.createElement("span");
+    elem.innerHTML = "Correct!";
+    elem.style.borderTop = "1px solid";
+    qTwo.appendChild(elem);
 }
